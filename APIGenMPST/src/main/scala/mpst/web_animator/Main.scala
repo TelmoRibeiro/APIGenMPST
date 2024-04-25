8120a03db8dd57e9e317dd7437185b478a95a988
Telmo André Azevedo Ribeiro <up201805124@up.pt>
2024-04-25 13:34:19 +0100

    frontend...

--- APIGenMPST/src/main/scala/mpst/web_animator/Main.scala
+++ APIGenMPST/src/main/scala/mpst/web_animator/Main.scala
@@ -8,149 +8,100 @@ import mpst.utilities.Environment.*
 import mpst.utilities.Multiset
 import mpst.utilities.Types.*
 import mpst.wellformedness.*
-
-import scala.annotation.tailrec
-import scala.io.StdIn
-
 import org.scalajs.dom
-import org.scalajs.dom.document
+
+import org.scalajs.dom.{HTMLParagraphElement, MouseEvent, document, html}
 
 object Main:
-  private def go(index:Int,nextNetwork:Set[(Action,Set[Protocol],Multiset[Action])],trace:List[Action])(using environment:Map[Variable,Protocol]):Unit =
-    val (a,ls,p) = nextNetwork.toSeq(index)
-    appendTextDiv(s"trace: ${trace :+ a}")
-    networkMSTraverse(ls,p,trace :+ a)
-  end go
+  private def removeButtons(except:Set[String]):Unit =
+    val buttons = document.querySelectorAll("button")
+    for button <- buttons if !(except contains button.id) yield
+      button.parentNode.removeChild(button)
+  end removeButtons
 
-  private def appendButton(label:String,index:Int,nextNetwork:Set[(Action,Set[Protocol],Multiset[Action])],trace:List[Action])(using environment:Map[Variable,Protocol]):Unit =
-    val body = document.body
-    val button = document.createElement("button").asInstanceOf[dom.html.Button]
+  private def addParagraph(text:String,parentNode:dom.HTMLElement):Unit =
+    val paragraph = dom.document.createElement("p").asInstanceOf[HTMLParagraphElement]
+    paragraph.textContent = text
+    parentNode.appendChild(paragraph)
+  end addParagraph
+
+  private def selectAction(buttonIndex:Int,trace:List[Action],network:Set[(Action,Set[Protocol],Multiset[Action])])(using environment:Map[Variable,Protocol]):Unit =
+    if network.isEmpty then
+      addParagraph(s"FINAL TRACE: $trace",document.body)
+      return
+    val (action,locals,pending) = network.toSeq(buttonIndex)
+    addParagraph(s"TRACE: ${trace :+ action}",document.body)
+    removeButtons(Set() + "protocolButton")
+    networkMSTraverse(locals,pending,trace :+ action)
+  end selectAction
+
+  private def addActionButton(label:String,index:Int,trace:List[Action],network:Set[(Action,Set[Protocol],Multiset[Action])])(using environment:Map[Variable,Protocol]):Unit =
+    val button = document.createElement("button").asInstanceOf[html.Button]
     button.textContent = label
-    button.onclick = (_:dom.MouseEvent) => {
-      go(index,nextNetwork,trace)
+    button.onclick     = (_:dom.MouseEvent) => {
+      selectAction(index,trace,network)
     }
-    body.appendChild(button)
-  end appendButton
+    document.body.appendChild(button)
+  end addActionButton
 
-  private def appendTextDiv(text:String):Any =
-    val body = document.body
-    val div  = document.createElement("div").asInstanceOf[dom.html.Div]
-    div.textContent = text
-    body.appendChild(div)
-    div
-  end appendTextDiv
-
-  // @tailrec
   private def networkMSTraverse(locals:Set[Protocol],pending:Multiset[Action],trace:List[Action])(using environment:Map[Variable,Protocol]):Unit =
     val nextNetwork = NetworkMultiset.nextNetwork(locals,pending)
     for local <- locals yield
-      appendTextDiv(s"local: $local")
-    appendTextDiv("")
-    var index = 0
+      addParagraph(s"LOCAL: $local",document.body)
+    var actionIndex = 0
     for (nextAction,_,_) <- nextNetwork yield
-      appendTextDiv(s"ACTION: $nextAction")
-      appendButton(nextAction.toString,index,nextNetwork,trace)
-      index += 1
-    if nextNetwork.toSeq.isEmpty then
-      appendTextDiv(s"final trace: $trace") ; return
-    // appendTextDiv(s"action index:")
+      addActionButton(nextAction.toString,actionIndex,trace,nextNetwork)
+      actionIndex += 1
   end networkMSTraverse
 
-  /*
-  type Queue = Map[(Agent,Agent),List[Message]]
-  @tailrec
-  private def networkCSTraverse(locals:Set[Protocol],pending:Queue,trace:List[Action])(using environment:Map[Variable,Protocol]):Unit =
-    val nextNetwork = NetworkCausal.nextNetwork(locals,pending)
-    for local <- locals yield
-      println(s"local: $local")
-    println()
-    for (nextAction,_,_) <- nextNetwork yield
-      println(s"ACTION: $nextAction\n")
-    if nextNetwork.toSeq.isEmpty then
-      println(s"final trace: $trace") ; return
-    println(s"action index:")
-    val actionIndex = StdIn.readInt( )
-    val (a,ls,p) = nextNetwork.toSeq(actionIndex)
-    println(s"trace: ${trace :+ a}")
-    networkCSTraverse(ls,p,trace :+ a)
-  end networkCSTraverse
-  */
+  private def showProtocol(protocol:String):Unit =
+    val protocolTitle = document.getElementById("protocolTitle").asInstanceOf[html.Title]
+    val protocolDiv   = showText("PROTOCOL",protocol)
+    protocolTitle.parentNode.insertBefore(protocolDiv,protocolTitle.nextSibling)
+  end showProtocol
 
-  def main(args: Array[String]): Unit =
-    val protocol = "m>wA:Work<void> ; m>wB:Work<void> ; (wA>m:Done<void> ; end || wB>m:Done<void> ; end)" // should and it is accepted
-    appendTextDiv(s"PROTOCOL: $protocol")
-    val global = Parser(protocol)
-    appendTextDiv(s"GLOBAL: $global")
-    val wellformed =  WellCommunicated(global) && WellBounded(global) && WellChannelled(global) && WellBranched(global) && DependentlyGuarded(global)
-    if !wellformed then throw new RuntimeException(s"not well formed!\n")
-    for agent -> local <- projectionWithAgent(global) yield
-      appendTextDiv(s"LOCAL [$agent] - $local")
-    val locals = projection(global)
-    val environment = getEnvironment(global)(using Map())
-    networkMSTraverse(locals,Multiset(),Nil)(using environment)
-  //networkCSTraverse(locals,Map(),Nil)(using environment)
-  end main
-end Main
+  private def showGlobal(global:String):Unit =
+    val globalTitle = document.getElementById("globalTitle").asInstanceOf[html.Title]
+    val globalDiv   = showText("GLOBAL",global)
+    globalTitle.parentNode.insertBefore(globalDiv,globalTitle.nextSibling)
+  end showGlobal
 
-/*
-package mpst.web_animator
+  private def showAnalysis(wellFormed:Boolean):Unit =
+    val analysisTitle = document.getElementById("analysisTitle").asInstanceOf[html.Title]
+    val analysisDiv   = showText("WELL-FORMED",wellFormed.toString)
+    analysisTitle.parentNode.insertBefore(analysisDiv,analysisTitle.nextSibling)
+  end showAnalysis
 
-import mpst.operational_semantic.Network.*
-import mpst.projection.Projection.*
-import mpst.syntax.*
-import mpst.utilities.Multiset
-import mpst.utilities.Types.*
-import mpst.utilities.Environment.*
-import mpst.wellformedness.*
+  private def showLocal(agent:String,local:String):Unit =
+    val localTitle = document.getElementById("localTitle").asInstanceOf[html.Title]
+    val localDiv   = showText(s"LOCAL [$agent]",local)
+    localTitle.parentNode.insertBefore(localDiv,localTitle.nextSibling)
+  end showLocal
 
-import org.scalajs.dom
-import org.scalajs.dom.document
-
-object Main:
-  private def show(text:String):Unit =
-    val body = document.body
-    val div = document.createElement("div")
-    div.textContent = text
-    body.appendChild(div)
-  end show
+  private def showText(label:String,description:String):html.Div =
+    val div = document.createElement("div").asInstanceOf[html.Div]
+    val labelSpan = document.createElement("span").asInstanceOf[html.Span]
+    labelSpan.textContent = label + ": "
+    labelSpan.style.color = "blue"
+    val descriptionSpan = document.createElement("span").asInstanceOf[html.Span]
+    descriptionSpan.textContent = description
+    descriptionSpan.style.color = "black"
+    div.appendChild(labelSpan)
+    div.appendChild(descriptionSpan)
+    div
+  end showText
 
   def main(args:Array[String]):Unit =
     val protocol = "m>wA:Work<void> ; m>wB:Work<void> ; (wA>m:Done<void> ; end || wB>m:Done<void> ; end)"
-    show(s"PROTOCOL: $protocol")
+    showProtocol(protocol)
     val global = Parser(protocol)
-    show(s"GLOBAL: $global")
-    val wellformed = WellCommunicated(global) && WellBounded(global) && WellChannelled(global) && WellBranched(global) && DependentlyGuarded(global)
-    if !wellformed then show(s"not wellformed!")
+    showGlobal(global.toString)
+    val wellFormed = WellCommunicated(global) && WellBounded(global) && WellChannelled(global) && WellBranched(global) && DependentlyGuarded(global)
+    showAnalysis(wellFormed)
     for agent -> local <- projectionWithAgent(global) yield
-      show(s"LOCAL [$agent] - $local")
-    def networkTraverse(locals:Set[Protocol],network:Multiset[Action],counter:Int,trace:List[Action])(using environment:Map[Variable,Protocol]):Unit =
-      show(s"TRACE: $trace")
-      if counter <= 0 then
-        show("OOPS, TO reached!")
-        return
-      val result = NetworkMultiset.next(locals,network)
-      for (nextAction,nextLocals,nextNetwork) <- result yield
-        show(s"CURRENT ENTRY")
-        show(s"Action Selected: $nextAction")
-        show(s"Locals State:")
-        for nextLocal <- nextLocals yield
-          show(s"Local: $nextLocal")
-        show(s"Network State: $nextNetwork")
-        show("\n")
-      show("PLEASE PICK ACTION:")
-      val submitButton = document.getElementById("submitButton")
-      submitButton.addEventListener("click", (event: dom.Event) => {
-        event.preventDefault()
-        val inputField = document.getElementById("inputField").asInstanceOf[dom.html.Input]
-        val inputValue = inputField.value
-        val integerValue = inputValue.toInt
-        val (a, ls, n) = result.toSeq(integerValue)
-        networkTraverse(ls, n, counter - 1, trace :+ a)
-      })
-    end networkTraverse
-    val locals = projection(global)
+      showLocal(agent,local.toString)
     val environment = getEnvironment(global)(using Map())
-    networkTraverse(locals,Multiset(),10,Nil)(using environment)
+    val locals      = projection(global)
+    networkMSTraverse(locals,Multiset(),Nil)(using environment)
   end main
 end Main
-*/
